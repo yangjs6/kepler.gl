12f78a3ce12eb884e61fcffd55558a13df10e60a
yangjs6 <276993857@qq.com>
2024-07-19 17:57:43 +0800

    添加 input-url 组件

---
+++ src/components/src/common/input-url.tsx
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the kepler.gl project
+
+import React, {Component} from 'react';
+
+function noop() {}
+
+interface InputUrlProps {
+  value?: string;
+  onChange?: (string) => void;
+}
+
+export default class InputUrl extends Component<InputUrlProps> {
+  static defaultProps = {
+    onChange: noop,
+    value: ''
+  };
+
+  state = {
+    websiteUrl: "",
+    isValid: false
+  };
+
+
+  validateWebsiteUrl = (urlString) => {
+    try { 
+      return Boolean(new URL(urlString)); 
+    }
+    catch(e){ 
+      return false; 
+    }
+  };
+
+  changeUrl = (e) => {
+    const { value } = e.target;
+    const isValid = !value || this.validateWebsiteUrl(value);
+
+    this.setState({
+      websiteUrl: value,
+      isValid,
+    });
+
+    if(this.props.onChange && isValid){
+        this.props.onChange(value);
+    }
+  };
+
+
+  render() {
+    return (
+        <form>
+          <input
+            type="text"
+            value={this.state.websiteUrl}
+            onChange={this.changeUrl}
+          />
+          {!this.state.isValid && (
+            <div style={{ color: "red" }}>URL is invalid</div>
+          )}
+        </form>
+    );
+  }
+}
