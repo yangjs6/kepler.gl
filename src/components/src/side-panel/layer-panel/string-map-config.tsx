6ad534eaf78fd4428d8364943474ae5002279b47
yangjs6 <276993857@qq.com>
2024-07-22 17:56:30 +0800

    添加 string-map-config 组件

---
+++ src/components/src/side-panel/layer-panel/string-map-config.tsx
@@ -0,0 +1,308 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the kepler.gl project
+
+import React, {Component, createRef, MouseEventHandler, MouseEvent, PropsWithChildren} from 'react';
+import classnames from 'classnames';
+import styled, {css} from 'styled-components';
+import {
+  SortableContainer,
+  SortableContainerProps,
+  SortableElement,
+  SortableElementProps,
+  SortableHandle
+} from 'react-sortable-hoc';
+// import Portaled from '../../common/portaled';
+
+import {Button, InlineInput} from '../../common/styled-components';
+import {VertDots, Trash} from '../../common/icons';
+// import ColorPalette from './color-palette';
+// import CustomPicker from './custom-picker';
+import {arrayMove} from '@kepler.gl/utils';
+// import {ColorRange} from '@kepler.gl/constants';
+import {NestedPartial, StringMap} from '@kepler.gl/types';
+
+
+type StringMapConfigProps = {
+    keyValues: StringMap;
+    setStringMap: (c: NestedPartial<StringMap>) => void;
+    // onToggleSketcher: (i: boolean | number) => void;
+    onReset?: (p: StringMap) => void;
+  };
+
+const dragHandleActive = css`
+  .layer__drag-handle {
+    color: ${props => props.theme.textColorHl};
+    opacity: 1;
+    cursor: move;
+  }
+`;
+
+const StyledSortableItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 6px;
+  padding-bottom: 6px;
+  z-index: ${props => props.theme.dropdownWrapperZ + 1};
+
+  :not(.sorting) {
+    :hover {
+      background-color: ${props => props.theme.panelBackgroundHover};
+      ${dragHandleActive};
+    }
+  }
+
+  &.sorting-colors {
+    background-color: ${props => props.theme.panelBackgroundHover};
+    ${dragHandleActive};
+  }
+`;
+
+const StyledDragHandle = styled.div`
+  display: flex;
+  align-items: center;
+  opacity: 0;
+`;
+
+const StyledTrash = styled.div`
+  color: ${props => props.theme.textColor};
+  svg {
+    :hover {
+      color: ${props => props.theme.subtextColorActive};
+    }
+  }
+  height: 12px;
+  margin-left: auto;
+  margin-right: 12px;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const StyledLine = styled.div`
+  width: calc(100% - 16px);
+  height: 1px;
+  background-color: ${props => props.theme.labelColor};
+  margin-top: 8px;
+  margin-left: 8px;
+`;
+
+// const StyledSwatch = styled.div.attrs({
+//   className: 'custom-palette__swatch'
+// })`
+//   background-color: ${props => props.color};
+//   width: 32px;
+//   height: 18px;
+//   display: inline-block;
+//   :hover {
+//     box-shadow: ${props => props.theme.boxShadow};
+//     cursor: pointer;
+//   }
+// `;
+
+// const StyledColorRange = styled.div`
+//   padding: 0 8px;
+//   :hover {
+//     background-color: ${props => props.theme.panelBackgroundHover};
+//     cursor: pointer;
+//   }
+// `;
+
+const StyledButtonContainer = styled.div`
+  margin-top: 11px;
+  display: flex;
+  direction: rtl;
+`;
+
+const StyledInlineInput = styled.div`
+  margin-left: 12px;
+  input {
+    color: ${props => props.theme.textColorHl};
+    font-size: 10px;
+  }
+`;
+
+type SortableItemProps = SortableElementProps & {
+  children?: React.ReactNode;
+  className?: string;
+  isSorting: boolean;
+};
+
+const SortableItem = SortableElement<SortableItemProps>(({children, isSorting}) => (
+  <StyledSortableItem
+    className={classnames('custom-palette__sortable-items', {sorting: isSorting})}
+  >
+    {children}
+  </StyledSortableItem>
+));
+
+type WrappedSortableContainerProps = SortableContainerProps & {
+  children?: React.ReactNode;
+  className?: string;
+};
+
+// TODO: Should className be applied to the div here?
+const WrappedSortableContainer = SortableContainer<WrappedSortableContainerProps>(
+  ({children, className}) => <div className={className}>{children}</div>
+);
+
+type DragHandleProps = PropsWithChildren<{className?: string; listeners?: unknown}>;
+
+export const DragHandle = SortableHandle<DragHandleProps>(({className, children}) => (
+  <StyledDragHandle className={className}>{children}</StyledDragHandle>
+));
+
+class StringMapConfig extends Component<StringMapConfigProps> {
+  state = {
+    isSorting: false
+  };
+
+  root = createRef<HTMLDivElement>();
+
+  _setStringMapUI(keys: string[], values: string[]) {
+    this.props.setStringMap({
+      keys,
+      values
+    });
+  }
+
+  _onKeyValueDelete = (index: number) => {
+    const {keys, values} = this.props.keyValues;
+    const newKeys = [...keys];
+    const newValues = [...values];
+    if (newKeys.length > 0) {
+      newKeys.splice(index, 1);
+    }
+    if (newValues.length > 0) {
+      newValues.splice(index, 1);
+    }
+    this._setStringMapUI(newKeys, newValues);
+  };
+
+  _onKeyValueAdd = () => {
+    const {keys, values} = this.props.keyValues;
+    const newKeys = [...keys, keys[keys.length - 1]];
+    const newValues = [...values, values[values.length - 1]];
+    this._setStringMapUI(newKeys, newValues);
+  };
+
+  // _onSwatchClick = (index: number) => {
+  //   this.props.onToggleSketcher(index);
+  // };
+
+  // _onSwatchClose = () => {
+  //   this.props.onToggleSketcher(false);
+  // };
+
+  _onClear: MouseEventHandler = event => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    this._setStringMapUI([],[]);
+  };
+
+  _onReset: MouseEventHandler = event => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (this.props.onReset){
+      this.props.onReset(this.props.keyValues);
+    }
+  };
+
+  _onSortEnd = ({oldIndex, newIndex}) => {
+    const {keys, values} = this.props.keyValues;
+    const newKeys = arrayMove(keys, oldIndex, newIndex);
+    const newValues = arrayMove(values, oldIndex, newIndex);
+    this._setStringMapUI(newKeys, newValues);
+    this.setState({isSorting: false});
+  };
+
+  _onSortStart = () => {
+    this.setState({isSorting: true});
+  };
+
+  _inputKey = (index: number, {target: {value}}) => {
+    const {keys, values} = this.props.keyValues;
+    const newKeys = [...keys];
+    newKeys[index] = value;
+    this._setStringMapUI(newKeys, values);
+  };
+
+  _inputValue = (index: number, {target: {value}}) => {
+    const {keys, values} = this.props.keyValues;
+    const newValues = [...values];
+    newValues[index] = value;
+    this._setStringMapUI(keys, newValues);
+  };
+
+  render() {
+    const {keys, values} = this.props.keyValues;
+
+    return (
+      <div className="custom-palette-panel" ref={this.root}>
+        <WrappedSortableContainer
+          className="custom-palette-container"
+          onSortEnd={this._onSortEnd}
+          onSortStart={this._onSortStart}
+          lockAxis="y"
+          helperClass="sorting-colors"
+          useDragHandle
+        >
+          {keys.map((key, index) => (
+            <SortableItem key={index} index={index} isSorting={this.state.isSorting}>
+              <DragHandle className="layer__drag-handle">
+                <VertDots height="20px" />
+              </DragHandle>
+              {/* <StyledSwatch color={color} onClick={() => this._onSwatchClick(index)} /> */}
+                
+              <StyledInlineInput>
+                <InlineInput
+                  type="text"
+                  className="custom-palette-hex__input"
+                  value={keys[index]}
+                  onClick={e => {
+                    e.stopPropagation();
+                  }}
+                  onChange={e => this._inputKey(index, e)}
+                  id={`input-layer-label-${index}`}
+                />
+              </StyledInlineInput>
+
+              <StyledInlineInput>
+                <InlineInput
+                  type="text"
+                  className="custom-palette-hex__input"
+                  value={values[index]}
+                  onClick={e => {
+                    e.stopPropagation();
+                  }}
+                  onChange={e => this._inputValue(index, e)}
+                  id={`input-layer-label-${index}`}
+                />
+              </StyledInlineInput>
+              <StyledTrash onClick={() => this._onKeyValueDelete(index)}>
+                <Trash className="trashbin" />
+              </StyledTrash>
+            </SortableItem>
+          ))}
+        </WrappedSortableContainer>
+        {/* Add Step Button */}
+        <Button className="add-step__button" link onClick={this._onKeyValueAdd}>
+          + Add Step
+        </Button>
+        <StyledLine />
+        {/* Cancel or Confirm Buttons */}
+        <StyledButtonContainer>
+          <Button className="confirm-apply__button" link onClick={this._onReset}>
+            Reset
+          </Button>
+          <Button link onClick={this._onClear}>
+            Clear
+          </Button>
+        </StyledButtonContainer>
+      </div>
+    );
+  }
+}
+
+export default StringMapConfig;
